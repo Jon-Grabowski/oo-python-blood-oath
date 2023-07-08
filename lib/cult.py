da960f5f8ff8cc8ed92a65b6eca4c00fd79a7869
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-08 17:41:28 -0400

    Cult advanced deliverables met

--- lib/cult.py
+++ lib/cult.py
@@ -1,5 +1,6 @@
 from .bloodoath import BloodOath
 
+
 class Cult:
     all = []
     
@@ -37,3 +38,39 @@ class Cult:
             if cult.founding_year == year:
                 return cult
         return f"No cults started in {year}"
+    
+    def average_age(self):
+        total_of_ages = 0
+        for follower in self.followers:
+            total_of_ages += follower.age
+        return total_of_ages/len(self.followers)
+    
+    def my_followers_mottos(self):
+        for follower in self.followers:
+            print(f"{follower.life_motto}")
+
+    def least_popular():
+        cult_followers = len(Cult.all[0].followers)
+        least_popular_cults = []
+        for cult in Cult.all:
+            if len(cult.followers) <= cult_followers:
+                cult_followers = len(cult.followers)
+        for cult in Cult.all:
+            if len(cult.followers) == cult_followers:
+                least_popular_cults.append(cult)
+        return least_popular_cults
+    #['San Diego, CA', 'Gainsville, FL', 'San Diego, CA']
+    def most_common_location():
+        list_of_locations = [cult.location for cult in Cult.all]
+        number_of_locations = 0
+        most_popular = []
+        for location in list_of_locations:
+            if list_of_locations.count(location) >= number_of_locations:
+                number_of_locations = list_of_locations.count(location)
+        for location in list_of_locations:
+            if list_of_locations.count(location) == number_of_locations and location not in most_popular:
+                most_popular.append(location)
+        return most_popular
+
+
+
